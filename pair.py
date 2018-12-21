59d59080c78e1c27ced3068496ded69efc29ac1e
Martin Höller <martin@helloclue.com>
2018-12-21 11:45:58 +0100

    deduplicate pairings; support for passing people list via command line params

--- pair.py
+++ pair.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+import sys
+
+
 def calculatePairs(people):
     evenNumber = (len(people) % 2 == 0)
 
@@ -30,7 +35,20 @@ def calculatePairs(people):
                 week_pairings.append(pair)
                 break
 
-    return pairings
+    # clean up pairings
+    cleaned_pairings = []
+    for week_pairing in pairings:
+        cleaned_week_pairings = []
+        for pair in week_pairing:
+            inverse_pair = (pair[1], pair[0])
+            if pair in cleaned_week_pairings or inverse_pair in cleaned_week_pairings:
+                continue
+            else:
+                cleaned_week_pairings.append(pair)
+
+        cleaned_pairings.append(cleaned_week_pairings)
+
+    return cleaned_pairings
 
 
 def weekContainsPartner(week_pairings, partner):
@@ -43,9 +61,23 @@ def pairingAlreadyExists(pairings, pairing):
     return pairing in flattened
 
 
-people = ["Kate", "Melissa", "Natalia", "Martin"]
+def printHelp():
+    print "Usage: "
+    print "\tpython pair.py person1 person2 ..."
+    print ""
+    print "Provide at least two persons"
+    print ""
+
+
+if len(sys.argv) < 3:
+    printHelp()
+    exit(0)
+
+people = sys.argv
+people.pop(0)
+
 pairings = calculatePairs(people)
 for index, week_pairings in enumerate(pairings):
     print("Week " + str(index + 1) + ":")
     for pair in week_pairings:
-        print("\t" + pair[0] + " + " + pair[1])
+        print("\t" + pair[0] + " / " + pair[1])
